00e7881f44847960064fb4814218d6369196b2de
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-06-08 18:35:55 +0100

    commit tree 769951b048432eb94f7e53f3ff36f0c293e78326

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+class Polygon {
+ constructor(array) {
+     this.sides = array
+ }
+
+ get countSides() {
+     return this.sides.length 
+ }
+
+ get perimeter() {
+    const totalPerim = (total, current) => total + current 
+    return (this.sides.reduce(totalPerim))
+ }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+      return (this.sides[0] + this.sides[1]) > this.sides[2] && (this.sides[1] + this.sides[2]) > this.sides[0]
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        return(this.sides[0] === this.sides[1] && this.sides[1] === this.sides[2] && this.sides[2] === this.sides[3])
+    }
+
+    get area() {
+        return this.sides[0]**2
+    }
+}
+
